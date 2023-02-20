f09b2ede63972923f7153f775d3cf7783f6f12d2
Alexey Kostyukov <support@pmg.ru>
2023-02-20 20:52:14 +0300

    task №4 update

--- task_4/solution.js
+++ task_4/solution.js
@@ -23,7 +23,7 @@ function renderProductsCards(json){
     let products = parseProducts(json);
     let length = products.length;
 
-    for (let i=0; i<(length-1); i+=1){
+    for (let i=0; i<=(length-1); i+=1){
         addProduct(products[i]);
     }
 
